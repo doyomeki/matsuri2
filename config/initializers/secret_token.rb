de5300c7400072d9c5f98688960964b8266eeff6
@kae_kasui <kaekasui_github@icloud.com>
2013-10-02 19:46:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Matsuri2::Application.config.secret_key_base = '837477f9c17204ad18730edd058c0eb0cf0dc44e9a6dc8a7decaaeea4d13f166e390abce2b3e4d3486619d0e035c85c2c785f9225aad9998febf2e333f771df8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Matsuri2::Application.config.session_store :cookie_store, key: '_matsuri2_session'
