7ce54354a24ac70d1debc4515d3aa72c74e682c1
@kae_kasui <kaekasui_github@icloud.com>
2013-10-24 00:38:27 +0900

    refs #6 add function of new contents

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -1,2 +1,13 @@
 class Admin::ContentsController < Admin::AdminBaseController
+
+  def create
+    @event = Event.find(params[:event_id])
+    @content = @event.contents.create(content_params)
+    redirect_to admin_event_path(@event.id)
+  end
+
+  private
+  def content_params
+    params[:content].permit(:title, :description, :speaker_name, :speaker_profile)
+  end
 end
