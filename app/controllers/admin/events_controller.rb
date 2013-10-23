cc3a9c47e3404b6c0799f859628b66f81fcd352b
koudaiii <cs006061@gmail.com>
2013-10-23 21:29:48 +0900

    refs #1 AddFunctionOfEvents

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -25,10 +25,9 @@ class Admin::EventsController < Admin::AdminBaseController
   # POST /events.json
   def create
     @event = Event.new(event_params)
-
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.html { redirect_to ["admin",@event], notice: 'Event was successfully created.' }
         format.json { render action: 'show', status: :created, location: @event }
       else
         format.html { render action: 'new' }
@@ -42,12 +41,29 @@ class Admin::EventsController < Admin::AdminBaseController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to ["admin",@event], notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
         format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+     @event.destroy
+      respond_to do |format|
+         format.html { redirect_to admin_events_path }
+         format.json { head :no_content }
+     end
+   end
+
+  private
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :capacity, :place, :start_datetime, :end_datetime, :deleted_at)
   end
 end
