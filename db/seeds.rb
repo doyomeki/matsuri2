5660a68a8db9281cf8bafdabec55728dc083987f
tenten0213 <takehito.0213@gmail.com>
2013-10-27 20:34:27 +0900

    refs #33 changed seeds dir

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@ require "csv"
 id = 1
 # Create Events
 Event.delete_all
-CSV.foreach('db/events.csv') do | row|
+CSV.foreach('db/seeds/events.csv') do | row|
   Event.create(id: id,title: row[0], description: row[1], capacity: row[2], place: row[3], start_at: row[4], finish_at: row[5])
 end
 
 # Create Contents
 Content.delete_all
-CSV.foreach('db/contents.csv') do | row|
+CSV.foreach('db/seeds/contents.csv') do | row|
   Content.create(id: id, event_id: row[0], booth_id: row[1], title: row[2], description: row[3], start_at: row[4], finish_at: row[5], speaker_name: row[6], speaker_profile: row[7], speaker_image: row[8])
   id += 1
 end
@@ -17,7 +17,7 @@ end
 # Create Booths
 id = 1
 Booth.delete_all
-CSV.foreach('db/booths.csv') do | row|
+CSV.foreach('db/seeds/booths.csv') do | row|
   Booth.create(id: id, name: row[0], capacity: row[1])
   id += 1
 end
@@ -25,7 +25,7 @@ end
 # Create Users
 id = 1
 User.delete_all
-CSV.foreach('db/users.csv') do | row|
+CSV.foreach('db/seeds/users.csv') do | row|
   User.create(id: id, email: row[0], password: row[1], admin: row[2], nickname: row[3])
   id += 1
 end
@@ -33,7 +33,7 @@ end
 # Create Entries
 id = 1
 Entry.delete_all
-CSV.foreach('db/entries.csv') do | row|
+CSV.foreach('db/seeds/entries.csv') do | row|
   Entry.create(id: id, content_id: row[0], user_id: row[1])
   id += 1
 end
