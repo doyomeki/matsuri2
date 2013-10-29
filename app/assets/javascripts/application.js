4d4bdd45399bf6108605978f76ab5537d764a49a
@kae_kasui <kaekasui_github@icloud.com>
2013-10-30 00:23:42 +0900

    refs #40 add jquery-ui-bootstrap with Twitter Bootstrap, and add button 'upcoming'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.ui.all
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
