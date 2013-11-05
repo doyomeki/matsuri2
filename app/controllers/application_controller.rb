796f30f92c89d6a5ca293a49454a07acce900fd9
koudaiii <cs006061@gmail.com>
2013-11-05 19:50:08 +0900

    heroku basic setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PW"] if Rails.env.production?
 end
