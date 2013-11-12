994a01cb33d24bb4a6a83c8bf3993cd22d9264f9
@kae_kasui <kaekasui_github@icloud.com>
2013-11-12 22:05:38 +0900

    refs #47 add devise error messages with layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PW"] if Rails.env.production?
+
+  def after_sign_out_path_for resource
+    new_user_session_path
+  end
 end
