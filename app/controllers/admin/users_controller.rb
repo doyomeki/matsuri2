5f567dec1e33eb3292939c4147d3bf2fc33dacfb
@kae_kasui <kaekasui_github@icloud.com>
2013-11-21 00:39:23 +0900

    refs #72 edit user profile, but current user logout

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::AdminBaseController
   before_action :set_users, only: [:index]
+  before_action :set_user, only: [:edit, :update]
 
   def index
   end
@@ -11,13 +12,14 @@ class Admin::UsersController < Admin::AdminBaseController
   end
 
   def update
+
     respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to admin_events_path, notice: I18n.t('flash.update_event') }
+      if @user.update_without_current_password(user_params)
+        format.html { redirect_to admin_users_path, notice: I18n.t('flash.update_user') }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -29,6 +31,10 @@ class Admin::UsersController < Admin::AdminBaseController
   end
 
   def user_params
-    params.require(:user).permit(:nickname, :code, :admin)
+    params.require(:user).permit(:code, :nickname, :admin, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
