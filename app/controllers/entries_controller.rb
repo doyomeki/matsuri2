84695313ba884f408b3363aed268b39664202050
UGA <s.ugagod@gmail.com>
2013-12-08 12:52:14 +0900

    mod entry_button

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,25 +1,42 @@
 class EntriesController < ApplicationController
   before_action :set_content, only: [:insert_and_delete]
   before_action :set_user, only: [:insert_and_delete]
-  before_action :delete, only: [:insert_and_delete]
 
   def insert_and_delete
-    Entry.create(id: @entry_id, content_id: @content.id, user_id: @user.id)
-    redirect_to root_path
+    if Entry.status(@content, @user) then
+      @entry = Entry.where(content_id: @content.id, user_id: @user.id).first
+      @another_content = Entry.another_status(@content)
+      @entry.destroy!
+      respond_to do |format|
+        @status = Entry.status(@content, @user)
+        format.js
+      end
+    else
+      delete
+      @entry = Entry.new(id: @entry_id, content_id: @content.id, user_id: @user.id)
+      respond_to do |format|
+        if @entry.save then
+          @status = Entry.status(@content, @user)
+          @another_content = Entry.another_status(@content)
+          format.js
+        end
+      end
+    end
   end
 
   private
     def set_content
       @content = Content.find(params[:content_id])
     end
+
     def set_user
       @user = User.find(params[:user_id])
     end
+
     def delete
       @entries = Entry.all
       @entries.each do |entry|
-        #binding.pry
-        if Content.find(entry.content_id).start_at === @content.start_at && entry.user_id.to_s === params[:user_id] then
+        if Content.find(entry.content_id).start_at == @content.start_at && entry.user_id.to_s == params[:user_id] then
           @entry_id = entry.id
           entry.destroy!
         end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,21 @@ class Entry < ActiveRecord::Base
   acts_as_paranoid
 
   belongs_to :content
+
+  def status(content, user)
+    Entry.where(content_id: content.id, user_id: user.id).present?
+  end
+
+  def self.status(content, user)
+    Entry.where(content_id: content.id, user_id: user.id).present? 
+  end
+
+  def self.another_status(content)
+    for another_content in Content.where(start_at: content.start_at) do
+      if another_content != content then
+        return another_content
+      end
+    end
+  end
+
 end
